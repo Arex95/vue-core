0f1fe83ae2cdb0b0946e37283b8973d7ab7c0956
arex95 <arex95@proton.me>
2025-03-07 12:29:07 -0600

    initial struct

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'Arex Vue Core',
+      fileName: (format) => `arex-vue-core.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
