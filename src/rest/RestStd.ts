3a00bef28c64ee376cc490e7c5a7f8e88a872e54
arex95 <arex95@proton.me>
2025-03-27 07:36:45 -0600

    refactor: rename getMany method to getAll for clarity

--- src/rest/RestStd.ts
+++ src/rest/RestStd.ts
@@ -35,7 +35,7 @@ export class RestStd {
      * @param options Additional options for the fetch composable.
      * @returns The result of the fetch composable (typically a promise).
      */
-    static getMany<T>(params?: Record<string, any>, options: object = {}) {
+    static getAll<T>(params?: Record<string, any>, options: object = {}) {
         return this.fetchComposable({
             method: 'GET',
             url: this.resource,
